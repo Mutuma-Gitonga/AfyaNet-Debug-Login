0eb9743d30f1397018467c5b9f327ce5fe5a59e5
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-21 11:56:43 +0300

    add rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_134504) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_085619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_134504) do
     t.string "password_digest"
     t.string "phone_number"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.integer "doctor_id"
+    t.integer "patient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
