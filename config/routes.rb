84d00b006088fc1ae052fdd4d1e7473dcf396688
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-22 15:54:55 +0300

    feat: add patient profile patch & delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   post "/patient_signup", to: "patients#create" 
   post "/patient_login", to: "patients_sessions#create" 
   get "/patient_profile", to: "patients#show"
+  patch "/patient_profile", to: "patients#update"
+  delete "/patient_profile", to: "patients#destroy"
 
 end
