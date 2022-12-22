0e1b6f3b47ae6ef80ffb1d3c4df13416ef64f328
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-22 18:56:12 +0300

    feat: add create action in doctors controller - signup

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,18 @@
 class DoctorsController < ApplicationController
-end
+  wrap_parameters format: []
+  skip_before_action :authorize, only: :create
+
+  # Create a doctor
+  def create 
+    @doctor = Doctor.create!(doctor_params)
+    cookies[:auth_token] = @doctor.auth_token
+    render json: @doctor, status: :created
+  end
+
+  private 
+  
+  def doctor_params 
+    params.permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
+  end
+  
+
