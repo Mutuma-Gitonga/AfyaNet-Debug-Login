3a6d762c6efbf3719489ef45d0f055e47854ea7e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-02 19:19:50 +0300

    review actions and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :doctor
     belongs_to :patient
+
+    validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "must be between 1 and 5" }
 end
