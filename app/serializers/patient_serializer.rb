57d93dfa917612c5f61eb38b16cd40a3b71d0428
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-02 19:22:58 +0300

    patient serializer

---
+++ app/serializers/patient_serializer.rb
@@ -0,0 +1,15 @@
+class PatientSerializer < ActiveModel::Serializer
+    attributes :id, :first_name, :last_name, :date_of_birth, :email, :phone_number, :location, :profile_picture, :auth_token, :password_reset_token, :password_reset_sent_at, :remember_me, :created_at, :updated_at, :password_digest, :age
+
+    has_many :messages
+    has_many :appointments
+    has_many :medical_records
+
+    # calculate the age of the patient
+    def age
+        now = Time.now.utc.to_date
+        now.year - object.date_of_birth.year - (object.date_of_birth.to_date.change(:year => now.year) > now ? 1 : 0)
+    end
+
+
+end
