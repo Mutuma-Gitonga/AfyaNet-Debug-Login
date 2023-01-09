311322e6eb8480f88cb31ba06c652cbcded10c76
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-09 21:26:31 +0300

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,39 @@ Specialty.create!([
 ])
 
 puts 'Specialties created!'
+
+# Doctor
+puts 'Creating Doctor'
+
+Doctor.create!([{
+  "first_name":"Ashley",
+    "last_name":"Smith",
+    "email":"ashley@gmail.com",
+    "phone_number":"0712345906",
+    "password":"@Ashley13",
+    "password_confirmation":"@Ashley13",
+    "licence_no": 54321,
+    "specialty_id":3,
+    "location":"Karen",
+    "days_available_weekly":"monday,tuesday,wednesday,thursday",
+    "contract_length":6,
+    "specific_days_times_available":"10am - 16:45pm"
+}])
+
+puts 'Doctor created!'
+
+# Patient
+puts 'Creating Patient'
+
+Patient.create!([{
+  "first_name": "Sylvia",
+    "last_name": "Kamau",
+    "email":"sylvia@gmail.com",
+    "phone_number": "0768372439",
+    "location":"Karen",
+    "date_of_birth":"01/07/1999",
+    "password":"@Sylvia33",
+    "password_confirmation":"@Sylvia33"
+} ])
+
+puts 'Patient created!'
