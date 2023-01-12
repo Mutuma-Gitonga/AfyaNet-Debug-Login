dbfeb7363c2e3e1921fd5e49d36198d6980162d9
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-12 12:44:37 +0300

    add id to doctors serializer

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,5 +1,5 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :first_name, :last_name, :email, :phone_number, :licence_no, :location, :specialty, :contract_length, :days_available_weekly, :specific_days_times_available, :engaged, :remember_me, :profile_picture, :specialty_id
+  attributes :id, :first_name, :last_name, :email, :phone_number, :licence_no, :location, :specialty, :contract_length, :days_available_weekly, :specific_days_times_available, :engaged, :remember_me, :profile_picture, :specialty_id
 
   has_many :appointments
   has_many :messages
