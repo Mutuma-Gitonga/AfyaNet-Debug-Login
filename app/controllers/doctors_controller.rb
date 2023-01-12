e495caa7118d8fd9a9ac5a5d0d5ae2f8789abedc
Mutuma Gitonga <mgitonga150@gmail.com>
2023-01-12 08:02:14 +0300

    feat: implementing authorization with jwt

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,33 +4,32 @@ class DoctorsController < ApplicationController
   skip_before_action :doctor_authorize , only: :create
 
   # Create a doctor
-  def create 
+  def create
     @doctor = Doctor.create!(doctor_params)
-    cookies[:auth_token] = @doctor.auth_token
     render json: @doctor, status: :created
   end
 
   # Show profile of currently logged in doctor
   def show 
-    render json: @current_doctor, status: :ok
+    render json: @doctor, status: :ok
   end
 
   # Update logged in/current doctor's attributes
   def update 
-    @current_doctor.update!(doctor_params)
-    render json: @current_doctor, status: :ok
+    @doctor.update!(doctor_params)
+    render json: @doctor, status: :ok
   end
 
   # Delete a logged in/current doctor's profile altogether
   def destory 
-    @current_doctor.destroy
+    @doctor.destroy
     render json: { message: "Doctor profile deleted!" }, status: :ok
   end
 
   private 
   
   def doctor_params 
-    params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number, :licence_no, :location, :specialty, :contract_length, :days_available_weekly, :specific_days_times_available, :engaged, :remember_me, :profile_picture, :specialty_id)
+    params.require(:doctor).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number, :licence_no, :location, :specialty, :contract_length, :days_available_weekly, :specific_days_times_available, :engaged, :remember_me, :profile_picture, :specialty_id)
   end
 
 end
---
+++ app/serializers/doctor_serializer.rb
@@ -0,0 +1,3 @@
+class DoctorSerializer < ActiveModel::Serializer
+  attributes :email
+end
