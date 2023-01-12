80f90334bb3682543edd2824c534fee916f6a23a
Mutuma Gitonga <mgitonga150@gmail.com>
2023-01-12 08:31:53 +0300

    update create action in doctor controller & serializer attributes in doctor serializer

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,3 +1,3 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :email
+  attributes :first_name, :last_name, :email, :phone_number, :licence_no, :location, :specialty, :contract_length, :days_available_weekly, :specific_days_times_available, :engaged, :remember_me, :profile_picture, :specialty_id
 end
